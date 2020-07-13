9ca0bfc3afc11483612fd8a297be8c1858deb775
FlashCoder101 <anishwestwindsor@gmail.com>
2020-07-13 12:21:17 -0400

    Began the process for adding the pdf portion of the site

--- la-vue/src/views/1984pdf.vue
+++ la-vue/src/views/1984pdf.vue
@@ -1,130 +1,103 @@
 <template>
-  <div id="pdfvuer">
-    <div id="buttons" class="ui grey three item inverted bottom fixed menu transition hidden">
-      <a class="item" @click="page > 1 ? page-- : 1">
-        <i class="left chevron icon"></i>
-        Back
-      </a>
-      <a class="ui active item">
-        {{page}} / {{ numPages ? numPages : '∞' }}
-      </a>
-      <a class="item" @click="page < numPages ? page++ : 1">
-        Forward
-        <i class="right chevron icon"></i>
-      </a>
-    </div>
-    <div id="buttons" class="ui grey three item inverted bottom fixed menu transition hidden">
-      <a class="item" @click="scale -= scale > 0.2 ? 0.1 : 0">
-        <i class="left chevron icon" />
-          Zoom -
-      </a>
-      <a class="ui active item">
-        {{ formattedZoom }} %
-      </a>
-      <a class="item" @click="scale += scale < 2 ? 0.1 : 0">
-        Zoom +
-        <i class="right chevron icon" />
-      </a>
-    </div>
-    <pdf :src="pdfdata" v-for="i in numPages" :key="i" :id="i" :page="i"
-      :scale.sync="scale" style="width:100%;margin:20px auto;">
-      <template slot="loading">
-        loading content here...
-      </template>
-    </pdf>
+  <div id="1984pdf">
+
+    <PDFViewer
+      v-bind="{url}"
+      @document-errored="onDocumentErrored"
+      >
+      <PDFUploader
+        v-if="enableUploader"
+        :documentError="documentError"
+        @updated="urlUpdated"
+        slot="header"
+        class="header-item"
+        />
+    </PDFViewer>
   </div>
 </template>
- 
+
 <script>
-import pdfvuer from 'pdfvuer'
- 
+import PDFViewer from './components/PDFViewer.vue'
 export default {
+  name: 'app',
   components: {
-    pdf: pdfvuer
+    PDFViewer
   },
-  data () {
+  data() {
     return {
-      page: 1,
-      numPages: 0,
-      pdfdata: undefined,
-      errors: [],
-      scale: 'page-width'
-    }
-  },
-  computed: {
-    formattedZoom () {
-        return Number.parseInt(this.scale * 100);
-    },
-  },
-  mounted () {
-    this.getPdf()
-  },
-  watch: {
-    show: function (s) {
-      if(s) {
-        this.getPdf();
-      }
-    },
-    page: function (p) {
-      if( window.pageYOffset <= this.findPos(document.getElementById(p)) || ( document.getElementById(p+1) && window.pageYOffset >= this.findPos(document.getElementById(p+1)) )) {
-        // window.scrollTo(0,this.findPos(document.getElementById(p)));
-        document.getElementById(p).scrollIntoView();
-      }
-    }
+      url: process.env.VUE_APP_PDF_URL,
+      documentError: undefined,
+      enableUploader: process.env.VUE_APP_UPLOAD_ENABLED === 'true',
+    };
   },
   methods: {
-    getPdf () {
-      var self = this;
-      self.pdfdata = pdfvuer.createLoadingTask('@/1984Book.pdf');
-      self.pdfdata.then(pdf => {
-        self.numPages = pdf.numPages;
-        window.onscroll = function() { 
-          changePage() 
-          stickyNav()  
-        }
- 
-        // Get the offset position of the navbar
-        var sticky = $('#buttons')[0].offsetTop
- 
-        // Add the sticky class to the self.$refs.nav when you reach its scroll position. Remove "sticky" when you leave the scroll position
-        function stickyNav() {
-          if (window.pageYOffset >= sticky) {
-            $('#buttons')[0].classList.remove("hidden")
-          } else {
-            $('#buttons')[0].classList.add("hidden")
-          }
-        }
- 
-        function changePage () {
-          var i = 1, count = Number(pdf.numPages);
-          do {
-            if(window.pageYOffset >= self.findPos(document.getElementById(i)) && 
-                window.pageYOffset <= self.findPos(document.getElementById(i+1))) {
-              self.page = i
-            }
-            i++
-          } while ( i < count)
-          if (window.pageYOffset >= self.findPos(document.getElementById(i))) {
-            self.page = i
-          }
-        }
-      });
+    urlUpdated(url) {
+      this.documentError = undefined;
+      this.url = url;
     },
-    findPos(obj) {
-      return obj.offsetTop;
-    }
-  }
+    onDocumentErrored(e) {
+      this.documentError = e.text;
+    },
+  },
 }
 </script>
- 
-<style lang="css" scoped>
-  #buttons {
-    margin-left: 0 !important;
-    margin-right: 0 !important;
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #606f7b;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #62637a;
+}
+label.form {
+  color: white;
+  font-family: Monaco, 'Courier New', Courier, monospace;
+  font-weight: bold;
+  margin-bottom: 2em;
+  display: block;
+}
+input {
+  padding: 0.45em;
+  font-size: 1em;
+}
+.error {
+  border: 1px solid red;
+  background: pink;
+  color: red;
+  padding: 0.5em 3em;
+  display: inline;
+}
+a.icon {
+  cursor: pointer;
+  display: block;
+  border: 1px #333 solid;
+  background: white;
+  color: #333;
+  font-weight: bold;
+  padding: 0.25em;
+  width: 1em;
+  height: 1em;
+  font-size: 1.5em;
+}
+.box-shadow {
+  box-shadow: 0 15px 30px 0 rgba(0, 0, 0, 0.11), 0 5px 15px 0 rgba(0, 0, 0, 0.08);
+}
+.overflow-hidden {
+  overflow: hidden;
+}
+@media print {
+  body {
+    background-color: transparent;
   }
-  /* Page content */
-  .content {
-    padding: 16px;
+  #app {
+    margin: 0;
+    padding: 0;
   }
+}
 </style>
- 
