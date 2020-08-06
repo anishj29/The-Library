c51f541ea8eb71fb148893500f283cab553cdb9b
FlashCoder101 <anishwestwindsor@gmail.com>
2020-08-06 12:00:54 -0400

    Added a filter for the pdf page for when a user clicks on either the character or locations buttons

--- la-vue/src/components/Bookpage.vue
+++ la-vue/src/components/Bookpage.vue
@@ -1,23 +1,26 @@
 <template>
-<v-container fluid class="px-12">
+
+<v-container fluid class="px-12 light-blue lighten-3">
 	<v-row class="pt-8">
 		<v-col cols="1">
 			<v-img max-width="100%" :src="require(`@/assets/images/${img}`)"></v-img>
 		</v-col>
 		<v-col>
-			<h1 class="text-center">{{title}}</h1>
+			<h1 class="text-left mt-10" style="padding-left: 600px">{{title}}</h1>
 		</v-col>
 	</v-row>
 	<v-row>
 		<v-spacer></v-spacer>
 		<v-col cols="4">
-			<v-text-field outlined dense label="Solo" placeholder="Search Bar" solo ></v-text-field>
+			<v-text-field class="" outlined dense label="Solo" placeholder="Search Bar" solo>
+			</v-text-field>
 		</v-col>
+	
 	</v-row>
 	<v-row class="pt-5">
 		<v-col cols="7"  class="blue-grey darken-4 white--text">
 			<div height="700">
-				<PDF1984></PDF1984>
+				<OdysseyBook></OdysseyBook>
 			</div>
 			
 		</v-col>
@@ -32,11 +35,9 @@
 				<v-expansion-panel class="blue-grey darken-4 white--text">
 					<v-expansion-panel-header>Filter</v-expansion-panel-header>
 					<v-expansion-panel-content>
-						<v-btn class="mr-1">Tags</v-btn>
-						<v-btn>Characters</v-btn>
-						<p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit
-						, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-						, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<v-btn class="mr-1" v-on:click="filter = locations">Locations</v-btn>
+						<v-btn v-on:click="filter = main_characters">Characters</v-btn>
+						<p class="mt-5">{{filter}}</p>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 			</v-expansion-panels>
@@ -46,18 +47,22 @@
 </template>
 
 <script>
-import PDF1984 from '@/components/1984pdf.vue'; 
+import OdysseyBook from '@/components/OdysseyBook.vue'; 
 export default {
 	components:{
-		PDF1984
+		OdysseyBook
 	},
-	props:{
-		bookObj: Object
-	},
+	// props:{
+	// 	bookObj: Object
+	// },
 	data: () => ({
 		img:"OdysseyCover.jpg",
 		title: "The Odyssey",
-		description: "The Odyssey is Homer's epic of Odysseus' 10-year struggle to return home after the Trojan War. While Odysseus battles mystical creatures and faces the wrath of the gods, his wife Penelope and his son Telemachus stave off suitors vying for Penelope's hand and Ithaca's throne long enough for Odysseus to return."
+		description: "The Odyssey is Homer's epic of Odysseus' 10-year struggle to return home after the Trojan War. While Odysseus battles mystical creatures and faces the wrath of the gods, his wife Penelope and his son Telemachus stave off suitors vying for Penelope's hand and Ithaca's throne long enough for Odysseus to return.",
+		filter: "Please select a filter",
+		main_characters: "Characters: Odysseus, Telemachus, Athena, Zeus, Circe, Calpyso, Poseidon...",
+		locations: "Troy, Calypso's Island, "
+	
 	})
 }
 </script>
---
+++ la-vue/src/components/OdysseyBook.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="OdysseyBook">
+    <iframe src="https://www.planetebook.com/free-ebooks/the-odyssey.pdf" width="100%" height="600px">
+    </iframe>
+  </div>
+</template>
+
+<script>
+
+</script>
